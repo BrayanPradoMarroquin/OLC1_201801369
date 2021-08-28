4005281a0c0a45f3ee6a0a63de856912e590fa2d
Brayan-PM <2995454660101@ingenieria.usac.edu.gt>
2021-08-27 22:09:24 -0600

    inicial

---
+++ FIUSAC Copy Analyzer/src/fiusac/copy/analyzer/FIUSACCopyAnalyzer.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fiusac.copy.analyzer;
+
+/**
+ *
+ * @author BRAYAN
+ */
+public class FIUSACCopyAnalyzer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Pantalla_Principal Principal = new Pantalla_Principal();
+        Principal.setVisible(true);
+    }
+    
+}
