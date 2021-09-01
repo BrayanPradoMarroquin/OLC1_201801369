38aa169f5044cc4c63ceeb710970d58f5bc2ddbe
Brayan-PM <2995454660101@ingenieria.usac.edu.gt>
2021-09-01 00:44:42 -0600

    Entrega de datos 3

--- FIUSAC Copy Analyzer/src/Analizadores/Sintactico.java
+++ FIUSAC Copy Analyzer/src/Analizadores/Sintactico.java
@@ -396,7 +396,8 @@ class CUP$Sintactico$actions {
 		int var1left = ((java_cup.runtime.Symbol)CUP$Sintactico$stack.elementAt(CUP$Sintactico$top-3)).left;
 		int var1right = ((java_cup.runtime.Symbol)CUP$Sintactico$stack.elementAt(CUP$Sintactico$top-3)).right;
 		String var1 = (String)((java_cup.runtime.Symbol) CUP$Sintactico$stack.elementAt(CUP$Sintactico$top-3)).value;
-		
+		graficapie.tipo=var1;
+                                                                GraficasEjecutar.add(graficapie);
               CUP$Sintactico$result = parser.getSymbolFactory().newSymbol("graficas",4, ((java_cup.runtime.Symbol)CUP$Sintactico$stack.elementAt(CUP$Sintactico$top-3)), ((java_cup.runtime.Symbol)CUP$Sintactico$stack.peek()), RESULT);
             }
           return CUP$Sintactico$result;
@@ -433,7 +434,8 @@ class CUP$Sintactico$actions {
 		int var1left = ((java_cup.runtime.Symbol)CUP$Sintactico$stack.elementAt(CUP$Sintactico$top-3)).left;
 		int var1right = ((java_cup.runtime.Symbol)CUP$Sintactico$stack.elementAt(CUP$Sintactico$top-3)).right;
 		String var1 = (String)((java_cup.runtime.Symbol) CUP$Sintactico$stack.elementAt(CUP$Sintactico$top-3)).value;
-		
+		graficapie.tipo=var1;
+                                                                GraficasEjecutar.add(graficapie);
               CUP$Sintactico$result = parser.getSymbolFactory().newSymbol("graficas",4, ((java_cup.runtime.Symbol)CUP$Sintactico$stack.elementAt(CUP$Sintactico$top-4)), ((java_cup.runtime.Symbol)CUP$Sintactico$stack.peek()), RESULT);
             }
           return CUP$Sintactico$result;
